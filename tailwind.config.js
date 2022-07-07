b0d664561efe949bb35a0efc191ff6ec560b2201
--global <manuel.bugarin@paymaya.com>
2022-07-07 22:43:28 +0800

    Partially converted blogpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
-      lexendDeca: ['Lexend Deca', 'sans-serif']
+      poppins: ["Poppins", "sans-serif"],
+      lexendDeca: ["Lexend Deca", "sans-serif"],
     },
-    extend: {}
+    gridTemplateColumns: {
+      "blog-card": "repeat(1fr, minmax(0, 2fr))",
+    },
+    colors: {
+      "navy-blue-400": "#415a77",
+      "navy-blue-600": "#1B263B",
+      "smoke-500": "#E0E1DD",
+    },
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
